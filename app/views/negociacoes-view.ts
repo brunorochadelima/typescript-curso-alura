01da4174dd58c033647de2d5c710e939ca1ebd67
Bruno Rocha de Lima <brunorocha33@live.com>
2022-08-03 13:13:59 -0300

    implementando classe abstrata

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,47 +1,39 @@
 import { Negociacoes } from "../models/negociacoes.js";
+import { View } from "./view.js";
 
-export class NegociacoesView {
-  private elemento: HTMLElement;
-  
-  constructor(seletor: string) {
-    this.elemento = document.querySelector(seletor);
-  }
-
-  //Cria o template da View
+export class NegociacoesView extends View<Negociacoes> {
   template(model: Negociacoes): string {
     return `
-    <table class="table table-hover table-bordered">
-      <thead>
-        <tr>
-          <th>Data</th>
-          <th>Quantidade</th>
-          <th>Valor</th>
-        </tr> 
-      </thead>
-      <tbody>
-      ${model.lista().map(negociacao => {
-        return `
-        <tr>
-          <td>
-           ${new Intl.DateTimeFormat().format(negociacao.data)}
-          </td>
-          <td>
-            ${negociacao.quantidade}
-          </td>
-          <td>
-          ${negociacao.valor}
-          </td>
-        </tr>
-        `
-      }).join('')}
-      </tbody>
-    </table>
-    `;
-  }
-
-  // renderiza o template no local informado pelo constructor
-  update(model: Negociacoes): void {
-    const template = this.template(model)
-    this.elemento.innerHTML = template;
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                </tr>
+            </thead>
+            <tbody>
+                ${model
+                  .lista()
+                  .map((negociacao) => {
+                    return `
+                        <tr>
+                            <td>${new Intl.DateTimeFormat().format(
+                              negociacao.data
+                            )}
+                            </td>
+                            <td>
+                                ${negociacao.quantidade}
+                            </td>
+                            <td>
+                                ${negociacao.valor}
+                            </td>
+                        </tr>
+                    `;
+                  })
+                  .join("")}
+            </tbody>
+        </table>
+        `;
   }
 }

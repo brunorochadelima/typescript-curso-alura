6853312186db8a01d7e5c8695de04f09caa8e476
Bruno Rocha de Lima <brunorocha33@live.com>
2022-08-04 08:48:48 -0300

    Método privado formatar data

--- dist/js/views/negociacoes-view.js
+++ dist/js/views/negociacoes-view.js
@@ -1,39 +1,38 @@
 import { View } from "./view.js";
 export class NegociacoesView extends View {
-    //Cria o template da View
     template(model) {
         return `
-    <table class="table table-hover table-bordered">
-      <thead>
-        <tr>
-          <th>Data</th>
-          <th>Quantidade</th>
-          <th>Valor</th>
-        </tr> 
-      </thead>
-      <tbody>
-      ${model.lista().map(negociacao => {
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                </tr>
+            </thead>
+            <tbody>
+                ${model
+            .lista()
+            .map((negociacao) => {
             return `
-        <tr>
-          <td>
-           ${new Intl.DateTimeFormat().format(negociacao.data)}
-          </td>
-          <td>
-            ${negociacao.quantidade}
-          </td>
-          <td>
-          ${negociacao.valor}
-          </td>
-        </tr>
+                        <tr>
+                            <td>${this.formatData(negociacao.data)}
+                            </td>
+                            <td>
+                                ${negociacao.quantidade}
+                            </td>
+                            <td>
+                                ${negociacao.valor}
+                            </td>
+                        </tr>
+                    `;
+        })
+            .join("")}
+            </tbody>
+        </table>
         `;
-        }).join('')}
-      </tbody>
-    </table>
-    `;
     }
-    // renderiza o template no local informado pelo constructor
-    update(model) {
-        const template = this.template(model);
-        this.elemento.innerHTML = template;
+    formatData(data) {
+        return new Intl.DateTimeFormat().format(data);
     }
 }
--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -2,4 +2,9 @@ export class View {
     constructor(seletor) {
         this.elemento = document.querySelector(seletor);
     }
+    // renderiza o template no local informado pelo constructor
+    update(model) {
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
 }

a0f53c4b817f79a2f687c6d4d17e9cf1fd70ad83
Bruno Rocha de Lima <brunorocha33@live.com>
2022-08-11 08:46:05 -0300

    Tratando null

--- app/app.ts
+++ app/app.ts
@@ -4,13 +4,17 @@ import { NegociacoesView } from "./views/negociacoes-view.js";
 const negociacao = new Negociacao(new Date(), 10, 100);
 console.log(negociacao.volume);
 
-const controller = new NegociacaoController()
-const form = document.querySelector('.form')
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  controller.adiciona()
-})
+const controller = new NegociacaoController();
+const form = document.querySelector(".form");
+if (form) {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    controller.adiciona();
+  });
+} else {
+  throw Error("Não foi possível inicializar a aplicação. Verifique se o form existe")
+}
 
-const negociacaoView = new NegociacoesView("#negociacoesView")
+const negociacaoView = new NegociacoesView("#negociacoesView");
 // const template = negociacaoView.template()
 // console.log(template)
